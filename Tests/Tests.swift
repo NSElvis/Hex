c492fefd3af14a19fd6bdf20ffc501c09f34b79d
Igor Ranieri Elland <igor@elland.me>
2016-06-15 11:36:12 +0200

    Convert to Swift 3.0 ✨✨
    - Broke a test, because of new colour space handling.

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -2,37 +2,38 @@ import UIKit
 import XCTest
 
 class Tests: XCTestCase {
+    // TODO: No longer returns a colour on extended the SRGBA space, so it no longer matches. Fix this somehow.
     func testBlackColor() {
         let blackHex = UIColor(hex: "000000")
-        let black = UIColor.blackColor()
+        let black = UIColor.black()
 
         XCTAssertTrue(blackHex.isEqualTo(black))
     }
 
     func testRedColor() {
         let redHex = UIColor(hex: "#ff0000")
-        let red = UIColor.redColor()
+        let red = UIColor.red()
 
         XCTAssertTrue(redHex.isEqualTo(red))
     }
 
     func testBlackAndWhiteColor() {
         let whiteHex = UIColor(hex: "FFFFFF")
-        let black = UIColor.blackColor()
+        let black = UIColor.black()
 
         XCTAssertFalse(whiteHex.isEqualTo(black))
     }
     
     func testTranslucentRedColor() {
         let redHex = UIColor(hex: "#ff000050")
-        let red = UIColor.redColor().colorWithAlphaComponent(0.5)
+        let red = UIColor.red().withAlphaComponent(0.5)
 
         XCTAssertTrue(redHex.isEqualTo(red))
     }
 
     func testClearColor() {
         let clearColorHex = UIColor(hex: "clearcolor")
-        let clear = UIColor.clearColor()
+        let clear = UIColor.clear()
 
         XCTAssertFalse(clearColorHex.isEqualTo(clear))
     }
